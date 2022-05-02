a6915103ec858be10217ae32b71fa435316c05fd
Filizzola <juliofilizzola@hotmail.com>
2022-05-02 11:44:56 -0300

    ⚡️ : populando o service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly repo: UsersRepository) {}
+  async create(createUserDto: CreateUserDto) {
+    const result = this.repo.create(createUserDto);
+    return result;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const result = this.repo.findAll();
+    return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const result = this.repo.findOne(id);
+    return result;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const result = this.repo.update(id, updateUserDto);
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const result = this.repo.remove(id);
+    return result;
   }
 }
