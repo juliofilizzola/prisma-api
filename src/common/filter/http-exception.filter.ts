6cb4c962f17723d147dfa87f11f68b008fddeecb
Filizzola <juliofilizzola@hotmail.com>
2022-05-04 10:23:50 -0300

    🥅 : implementando função para tratar o erro.

--- src/common/filter/http-exception.filter.ts
+++ src/common/filter/http-exception.filter.ts
@@ -1,6 +1,35 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+
+import { response as res } from 'express';
 
 @Catch()
-export class HttpExceptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+export class HttpExceptionFilter<T extends HttpException>
+  implements ExceptionFilter
+{
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const RES = ctx.getResponse<Response>();
+    const REQ = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const exceptionRes = exception.getResponse();
+
+    const error =
+      typeof RES === 'string'
+        ? { message: exceptionRes }
+        : (exceptionRes as object);
+
+    const resultErro = {
+      ...error,
+      statusCode: status,
+      date: new Date().toISOString(),
+      path: REQ.url,
+    };
+
+    res.status(status).json(resultErro);
+  }
 }
